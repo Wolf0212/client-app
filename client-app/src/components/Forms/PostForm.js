7b7a5a7721b8e9f52a6be91cb64f8aabb1888f10
phamsn2001 <phamsn2001@gmail.com>
2022-07-15 23:15:59 +0700

    Call API Post form

--- client-app/src/components/Forms/PostForm.js
+++ client-app/src/components/Forms/PostForm.js
@@ -31,7 +31,7 @@ const PostForm = () => {
     }
 
     if (attachmentFile.type.includes("image")) {
-      temp = <img alt="" src={attachmentFile.link.toString()} height="200" />;
+      temp = <img alt="" src={attachmentFile.link.toString()} width="320" />;
     }
     return temp;
   };
@@ -43,29 +43,32 @@ const PostForm = () => {
 
     uploadTask.on("state_changed", () => {
       return getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-        return url;
+        const object = {
+          title: title.current.value,
+          description: description.current.value,
+          attachmentType: attachmentFile.type.includes("image") ? 2 : 1,
+          fileURL: url,
+          preview: preview.current.value,
+        };
+        console.log(object);
+        AddPost(object);
       });
     });
   };
 
   async function SubmitFormHandler(e) {
     e.preventDefault();
-    console.log(attachmentFile.file);
-    const url = await UploadFile(attachmentFile.file);
-    const object = {
-      title: title.current.value,
-      description: description.current.value,
-      attachmentType: attachmentFile.includes("image") ? 2 : 1,
-      fileURL: url,
-      preview: preview.current.value,
-    };
-    AddPost(object);
+    UploadFile(attachmentFile.file);
   }
 
   function AddPost(o) {
     return axios
-      .post("https://localhost:44321/odata/Posts", o)
-      .then(alert("Success"));
+      .post("https://localhost:44321/odata/Posts", o, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((response) => console.log(response));
   }
 
   function PreviewAttachmentHandler(e) {
@@ -80,70 +83,71 @@ const PostForm = () => {
     };
   }
 
-  //   useEffect(() => {
-  //     if(attachmentFiles){
-  //        for(var i in attachmentFiles){
-  //        }
-  //     }
-  //   }, attachmentFiles);
+  const setAxiosDefaultHeader = () => {
+    axios.defaults.headers = {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    };
+  };
+
+  useEffect(() => {
+    setAxiosDefaultHeader();
+  });
 
   return (
-    <>
-      <form onSubmit={SubmitFormHandler}>
-        <FormControl fullWidth margin="dense">
-          <InputLabel htmlFor="title">Title</InputLabel>
-          <Input
-            id="title"
-            name="title"
-            placeholder="Title of the post"
-            type="text"
-            inputRef={title}
-          ></Input>
+    <form onSubmit={SubmitFormHandler}>
+      <FormControl fullWidth margin="dense">
+        <InputLabel htmlFor="title">Title</InputLabel>
+        <Input
+          id="title"
+          name="title"
+          placeholder="Title of the post"
+          type="text"
+          inputRef={title}
+        ></Input>
+      </FormControl>
+      <FormControl fullWidth margin="dense">
+        <InputLabel htmlFor="description">Description</InputLabel>
+        <Input
+          id="description"
+          name="description"
+          placeholder="Description of the post"
+          type="text"
+          multiline
+          inputRef={description}
+        ></Input>
+      </FormControl>
+      <FormControl fullWidth margin="dense">
+        <InputLabel htmlFor="preview">Preview </InputLabel>
+        <Input
+          inputRef={preview}
+          id="preview"
+          name="preview"
+          type="text"
+          multiline
+        ></Input>
+      </FormControl>
+      <div>
+        <FormControl margin="dense">
+          <Button variant="contained" component="label">
+            Upload file
+            <input
+              ref={attachment}
+              hidden
+              accept="*/*"
+              multiple
+              type="file"
+              onChange={PreviewAttachmentHandler}
+            />
+          </Button>
         </FormControl>
-        <FormControl fullWidth margin="dense">
-          <InputLabel htmlFor="description">Description</InputLabel>
-          <Input
-            id="description"
-            name="description"
-            placeholder="Description of the post"
-            type="text"
-            multiline
-            inputRef={description}
-          ></Input>
-        </FormControl>
-        <FormControl fullWidth margin="dense">
-          <InputLabel htmlFor="preview">Preview </InputLabel>
-          <Input
-            inputRef={preview}
-            id="preview"
-            name="preview"
-            type="text"
-            multiline
-          ></Input>
-        </FormControl>
+      </div>
+      {attachmentFile.link != null && (
         <div>
-          <FormControl margin="dense">
-            <Button variant="contained" component="label">
-              Upload file
-              <input
-                ref={attachment}
-                hidden
-                accept="*/*"
-                multiple
-                type="file"
-                onChange={PreviewAttachmentHandler}
-              />
-            </Button>
-          </FormControl>
+          <PreviewFile></PreviewFile>
         </div>
-        {attachmentFile.link != null && (
-          <div>
-            <PreviewFile></PreviewFile>
-          </div>
-        )}
-        <Button type="submit">Submit</Button>
-      </form>
-    </>
+      )}
+      <Button type="submit">Submit</Button>
+    </form>
   );
 };
 
